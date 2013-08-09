6fef50e6b8ea12aee076347cc16ec42bb99bf213
Ruchir Khaitan <rk2660@columbia.edu>
2013-08-08 22:07:49 -0400

    added code for pascals triangle

---
+++ 2013.8/pascals_triangle/pascal.c
@@ -0,0 +1,33 @@
+/* Code to print the first n rows of pascals triangle
+ * also stores the nth roow in memory as an array
+ * by Ruchir Khaitan
+ */
+
+#include <stdlib.h>
+#include <stdio.h> 
+
+int main(int argc, char** argv ) {
+    int rowNum = atoi(argv[1]) + 1;
+    unsigned long* array = malloc(sizeof(long) *rowNum);
+
+    int i, j;
+    unsigned long* curr;
+    for(i = 0; i<rowNum; i++) { 
+        curr = array + i;
+        *curr = 1; 
+        while (--curr > array) {
+            *curr += *(curr-1);
+        }
+        //printf("[");
+        for(j = 0; j<i; j++) {
+            printf("%u ", *(array+j));
+        }
+        printf("%u\n", *(array+ i));
+  }
+
+    free(array);
+
+}
+
+
+
