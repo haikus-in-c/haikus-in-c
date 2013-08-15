03398c8aee50e77163e234278117461dd78f984c
Ruchir Khaitan <rk2660@columbia.edu>
2013-08-15 02:19:40 +0000

    modified combinations code - works, but not on large enough inputs

--- 2013.8/combinations/combinations.c
+++ 2013.8/combinations/combinations.c
@@ -2,6 +2,42 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+unsigned long comb_pascal(int rowNum,int r, int x){
+
+    rowNum +=1;
+    unsigned long * array = (unsigned long *) malloc(sizeof(unsigned long) *(rowNum));
+
+    int i;
+    unsigned long* curr;
+    for(i = 0; i<rowNum; i++) {
+        curr = array + i;
+        *curr = 1.0;
+        while (--curr > array) {
+            *curr += (*(curr-1)%x);
+        }
+    }
+
+
+    unsigned long ans= array[r]%x;
+    free(array);
+    return ans;
+}
+
+long double comb_rec(int n, int r) {
+        if (r == 0 || n == 0)
+                return 1.0;
+        long double ndub = (long double) n;
+        long double rdub = (long double) r;
+        return (ndub/rdub) * comb_rec(n-1, r-1);
+
+}
+
+double comb_hack (int n , int r) {
+        double real_ans = comb_rec(n, r);
+        //real_ans = fmod(real_ans, 142857.0);
+        return real_ans;
+}
+
 unsigned long int comb2(int n, int r){
 	unsigned long int ans = 1; 
 	int i, j;
@@ -48,9 +84,9 @@ int main() {
         fgets(buf, 256, stdin);
 	sscanf(buf, "%d %d", &n, &r);
             printf("%d %d\n", n, r);
-            answer = combination(n,r);
+            answer = comb_pascal(n,r, 142857);
             ans2 = comb2(n, r);
-            printf("%lu %lu\n", answer, ans2);        
+            printf("%lu\n", answer);        
     }
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
